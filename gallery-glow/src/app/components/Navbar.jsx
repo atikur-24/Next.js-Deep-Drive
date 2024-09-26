b80b25060b14ec6eaa9ad5e941bf38292210ce36
atikur-24 <atikurrahman7223@gmail.com>
2024-09-26 15:19:44 +0600

    create navbar

---
+++ gallery-glow/src/app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="py-4 md:py-6 border-b">
+      <div className="container mx-auto flex items-center justify-between gap-x-6">
+        <h1 className="font-extrabold text-3xl bg-gradient-to-r inline-block text-transparent bg-clip-text from-cyan-500 to-blue-500">GalleryGlow</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
