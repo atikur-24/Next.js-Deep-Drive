ef04f14681df3f589db52e0730b742c31a948f18
atikur-24 <atikurrahman7223@gmail.com>
2025-12-17 23:30:42 +0600

    create layout

--- gallery-glow/src/app/layout.js
+++ gallery-glow/src/app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "PhotoFeed",
+  title: "Gallery Glow",
   description: "A Photo Feed App built with Next.js",
 };
 
