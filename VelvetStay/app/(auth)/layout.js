628b9322e6266284c0de23bd80ebc92afa2a7c54
atikur-24 <atikurrahman7223@gmail.com>
2026-07-29 09:52:44 +0600

    feat: Update authentication handling in layout and booking pages

--- VelvetStay/app/(auth)/layout.js
+++ VelvetStay/app/(auth)/layout.js
@@ -1,10 +1,18 @@
+import { auth } from "@/auth";
 import Navbar from "@/components/Navbar";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Auth - Velvet Stay",
 };
 
-export default function AuthLayout({ children }) {
+export default async function AuthLayout({ children }) {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <>
       <Navbar sideMenu={false} />
