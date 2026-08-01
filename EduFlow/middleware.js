b0a147f2a35c2f3765606408f911fa07abb5d2c8
atikur-24 <atikurrahman7223@gmail.com>
2026-08-01 23:36:46 +0600

    update middleware

--- EduFlow/middleware.js
+++ EduFlow/middleware.js
@@ -1,8 +1,7 @@
-
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-import {DEFAULT_REDIRECT, PUBLIC_ROUTES, LOGIN, ROOT} from "@/lib/routes";
+import { PUBLIC_ROUTES, ROOT } from "@/lib/routes";
 
 const { auth } = NextAuth(authConfig);
 
@@ -13,16 +12,13 @@ export default auth((req) => {
 
   console.log(isAuthenticated, nextUrl.pathname);
 
-  const isPublicRoute = (PUBLIC_ROUTES.find(route => nextUrl.pathname.startsWith(route))
-   || nextUrl.pathname === ROOT);
+  const isPublicRoute = PUBLIC_ROUTES.find((route) => nextUrl.pathname.startsWith(route)) || nextUrl.pathname === ROOT;
 
-  console.log({isPublicRoute});
+  console.log({ isPublicRoute });
 
-  if (!isAuthenticated && !isPublicRoute)
-    return Response.redirect(new URL(LOGIN, nextUrl));
+  if (!isAuthenticated && !isPublicRoute) return Response.redirect(new URL(LOGIN, nextUrl));
+});
 
- });
-
- export const config = {
+export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
- };
+};
