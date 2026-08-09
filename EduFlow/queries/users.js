67d379b57440d2680b95f366a1339b24c1c34354
atikur-24 <atikurrahman7223@gmail.com>
2026-08-09 09:59:40 +0600

    feat: Implement user profile management with personal details, contact info, and password change functionality

--- EduFlow/queries/users.js
+++ EduFlow/queries/users.js
@@ -1,10 +1,21 @@
 import { replaceMongoIdInObject } from "@/lib/convertData";
 import { User } from "@/model/user-model";
+import bcrypt from "bcryptjs";
 
 export async function getUserByEmail(email) {
   try {
     const user = await User.findOne({ email: email }).lean();
     return replaceMongoIdInObject(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function validatePassword(email, password) {
+  try {
+    const user = await getUserByEmail(email);
+    const isMatch = await bcrypt.compare(password, user.password);
+    return isMatch;
   } catch (error) {
     console.log(error);
   }
